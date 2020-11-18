afc6801018b87bae09368efece4c3908272608bb
Jayden Platell <jplatell@shelterbuddy.com>
2020-11-18 16:24:48 +1000

    Basic application to begin replicating packages

---
+++ Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<ListPackagesCommand>();
+
+            using var scope = serviceCollection.BuildServiceProvider().CreateScope();
+            var listPackagesCommand = scope.ServiceProvider.GetRequiredService<ListPackagesCommand>();
+            await listPackagesCommand.Execute(CancellationToken.None);
+        }
+    }
+}
